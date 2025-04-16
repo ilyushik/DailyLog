b6bfe95334e55188f6a3629bc7e0e714c8bbe58d
ilyushik <kamaraliilya@gmail.com>
2025-04-16 15:57:07 +0200

    calendar styles

--- Front-end/my-app/src/App.js
+++ Front-end/my-app/src/App.js
@@ -17,16 +17,16 @@ const PrivateRoute = ({ children }) => {
 function App() {
 
   return (
-    <Layout>
-        <Routes>
-            <Route exact path="/" element={<PrivateRoute><MainScreen /></PrivateRoute>} />
-            <Route path="/login" element={ <Login/> }/>
-            <Route exec path="/my-info" element={<PrivateRoute><MainScreen /></PrivateRoute>} />
-            <Route path="/inbox" element={ <PrivateRoute><Inbox/></PrivateRoute> } />
-            <Route path="/people" element={<PrivateRoute><PeopleList/></PrivateRoute>}/>
-            <Route path="/user/:id" element={<PrivateRoute><MainScreen /></PrivateRoute>}/>
-        </Routes>
-    </Layout>
+      <Layout>
+          <Routes>
+              <Route exact path="/" element={<PrivateRoute><MainScreen /></PrivateRoute>} />
+              <Route path="/login" element={ <Login/> }/>
+              <Route exec path="/my-info" element={<PrivateRoute><MainScreen /></PrivateRoute>} />
+              <Route path="/inbox" element={ <PrivateRoute><Inbox/></PrivateRoute> } />
+              <Route path="/people" element={<PrivateRoute><PeopleList/></PrivateRoute>}/>
+              <Route path="/user/:id" element={<PrivateRoute><MainScreen /></PrivateRoute>}/>
+          </Routes>
+      </Layout>
   );
 }
 

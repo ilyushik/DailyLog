7b33e9fa2401fd1284490cc4ee584b5a04c98d91
ilyushik <kamaraliilya@gmail.com>
2025-07-12 12:04:28 +0200

    getMyInfo, users integration tests

--- Back-end/SpringApp/src/test/java/org/example/springapp/Integration/UserControllerIntegrationTest.java
+++ Back-end/SpringApp/src/test/java/org/example/springapp/Integration/UserControllerIntegrationTest.java
@@ -16,7 +16,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDate;
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
@@ -25,77 +26,97 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserControllerIntegrationTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private ReportRepository reportRepository;
-    @Autowired
-    private RequestRepository requestRepository;
-    @Autowired
-    private UserRoleRepository userRoleRepository;
-    @Autowired
-    private RequestStatusRepository requestStatusRepository;
-    @Autowired
-    private ApproverActionRepository approverActionRepository;
-    @Autowired
-    private RequestReasonRepository requestReasonRepository;
-
-    @BeforeAll
-    public void init() {
-        RequestStatus status = new RequestStatus("Pending");
-        requestStatusRepository.save(status);
-        ApproverAction action = new ApproverAction("Unchecked");
-        approverActionRepository.save(action);
-        RequestReason reason = new RequestReason("Sick Leave");
-        requestReasonRepository.save(reason);
-        UserRole lead =  new UserRole("ROLE_LEAD");
-        UserRole userRole = new UserRole("ROLE_USER");
-        userRoleRepository.saveAll(List.of(userRole, lead));
-
-        User leadUser = new User();
-        leadUser.setEmail("testlead@example.com");
-        leadUser.setDaysForVacation(20);
-        leadUser.setDaysToSkip(3);
-        leadUser.setRole(userRole);
-        userRepository.save(leadUser);
-
-        User user = new User();
-        user.setEmail("test@example.com");
-        user.setDaysForVacation(20);
-        user.setDaysToSkip(3);
-        user.setRole(userRole);
-        user.setTeamLead(leadUser);
-        userRepository.save(user);
-    }
-
-    @BeforeEach
-    public void setUp() {
-    }
-
-    @Test
-    @WithMockUser(username = "test@example.com")
-    public void addRequest() throws Exception {
-        RequestDTO requestDTO = new RequestDTO();
-        requestDTO.setStartDate(LocalDate.of(2025, 7, 14));
-        requestDTO.setFinishDate(LocalDate.of(2025, 7, 14));
-        requestDTO.setReason("Sick Leave");
-        requestDTO.setComment("integration test");
-
-        String json = objectMapper.writeValueAsString(requestDTO);
-
-        mockMvc.perform(post("/addRequest")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json))
-                .andExpect(status().isOk());
-
-        log.info("\n\n\nAmount of requests: " + requestRepository.findAll().size() + "\n\n\n");
-
-        Assertions.assertEquals(1, requestRepository.findAll().size());
-    }
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//    @Autowired
+//    private ReportRepository reportRepository;
+//    @Autowired
+//    private RequestRepository requestRepository;
+//    @Autowired
+//    private UserRoleRepository userRoleRepository;
+//    @Autowired
+//    private RequestStatusRepository requestStatusRepository;
+//    @Autowired
+//    private ApproverActionRepository approverActionRepository;
+//    @Autowired
+//    private RequestReasonRepository requestReasonRepository;
+//
+//    @BeforeAll
+//    public void init() {
+//        RequestStatus status = new RequestStatus("Pending");
+//        requestStatusRepository.save(status);
+//        ApproverAction action = new ApproverAction("Unchecked");
+//        approverActionRepository.save(action);
+//        RequestReason reason = new RequestReason("Sick Leave");
+//        requestReasonRepository.save(reason);
+//        UserRole lead =  new UserRole("ROLE_LEAD");
+//        UserRole userRole = new UserRole("ROLE_USER");
+//        userRoleRepository.saveAll(List.of(userRole, lead));
+//
+//        User leadUser = new User();
+//        leadUser.setFirstName("Eve");
+//        leadUser.setSecondName("Davies");
+//        leadUser.setEmail("testlead@example.com");
+//        leadUser.setDaysForVacation(20);
+//        leadUser.setDaysToSkip(3);
+//        leadUser.setRole(lead);
+//        userRepository.save(leadUser);
+//
+//        User user = new User();
+//        user.setFirstName("Illia");
+//        user.setSecondName("Kamarali");
+//        user.setEmail("test@example.com");
+//        user.setDaysForVacation(20);
+//        user.setDaysToSkip(3);
+//        user.setRole(userRole);
+//        user.setTeamLead(leadUser);
+//        userRepository.save(user);
+//    }
+//
+//    @BeforeEach
+//    public void setUp() {
+//    }
+//
+//    @Test
+//    @WithMockUser("test@example.com")
+//    public void getMyInfo() throws Exception {
+//        mockMvc.perform(get("/getMyInfo"))
+//                .andDo(print())
+//                .andExpect(status().isOk());
+//    }
+//
+//    @Test
+//    @WithMockUser("test@example.com")
+//    public void users() throws Exception{
+//        mockMvc.perform(get("/users"))
+//                .andDo(print())
+//                .andExpect(status().isOk());
+//    }
+//
+//    @Test
+//    @WithMockUser(username = "test@example.com")
+//    public void addRequest() throws Exception {
+//        RequestDTO requestDTO = new RequestDTO();
+//        requestDTO.setStartDate(LocalDate.of(2025, 7, 14));
+//        requestDTO.setFinishDate(LocalDate.of(2025, 7, 14));
+//        requestDTO.setReason("Sick Leave");
+//        requestDTO.setComment("integration test");
+//
+//        String json = objectMapper.writeValueAsString(requestDTO);
+//
+//        mockMvc.perform(post("/addRequest")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(json))
+//                .andExpect(status().isOk());
+//
+//        log.info("\n\n\nAmount of requests: " + requestRepository.findAll().size() + "\n\n\n");
+//
+//        Assertions.assertEquals(1, requestRepository.findAll().size());
+//    }
 }
